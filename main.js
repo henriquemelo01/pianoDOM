ae1b9a901fe2d318d7cae76428e0849f36dcfe6b
Henrique Moraes <henriquemsm@outlook.com>
2020-12-23 11:49:18 -0300

    Get data key + Check if key pressed is on keyboard

--- main.js
+++ main.js
@@ -7,7 +7,22 @@ const keys = document.querySelectorAll(".key");
 // Add click event for all elements that have the key class. We could also use the array's "for each" method.
 
 const playNote = function (event) {
-  console.log(event.type);
+  // Get data-key for identify which key was pressed:
+  let keyCode;
+  if (event.type === "click") {
+    keyCode = event.target.dataset.key;
+  } else if (event.type === "keydown") {
+    keyCode = event.keyCode;
+  }
+
+  // check if key pressed is on keyboard:
+  const keyBoard = document.querySelector(`.key[data-key="${keyCode}" ]`);
+  const isPianoKey = keyBoard ? true : false;
+  if (!isPianoKey) {
+    return;
+  } else {
+    console.log(keyCode);
+  }
 };
 
 for (let i = 0; i < keys.length; i++) {
